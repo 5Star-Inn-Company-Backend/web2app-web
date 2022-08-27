60e68a3959539c2cd1d1fef78dfe94e8b5a81b9c
odejinmisa <odejinmisa@newwavesecosystem.com>
2022-08-27 10:51:13 +0100

    push changes

--- resources/views/emails/app_ready.blade.php
+++ resources/views/emails/app_ready.blade.php
@@ -4,23 +4,23 @@
 Your website has been converted to app as requested.
 Find the attached files.
 
-{{--@if($apk != "")--}}
-{{--    @component('mail::button', ['url' => $apk])--}}
-{{--    Download APK--}}
-{{--    @endcomponent--}}
-{{--@endif--}}
+@if($apk != "")
+@component('mail::button', ['url' => $apk])
+Download APK
+@endcomponent
+@endif
 
-{{--@if($aab != "")--}}
-{{--    @component('mail::button', ['url' => $aab])--}}
-{{--    Download AAB--}}
-{{--    @endcomponent--}}
-{{--@endif--}}
+@if($aab != "")
+@component('mail::button', ['url' => $aab])
+Download AAB
+@endcomponent
+@endif
 
-{{--@if($ios != "")--}}
+@if($ios != "")
 @component('mail::button', ['url' => $ios])
 Download Runner.app.zip
 @endcomponent
-{{--@endif--}}
+@endif
 
 Thanks,<br>
 {{ config('app.name') }}
