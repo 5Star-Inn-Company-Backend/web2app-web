5d1f49217c0c287449885e0ecb607631f2bfe5f8
Timtech-wiz <ajetetimothysamuel@gmail.com>
2024-11-18 21:58:41 +0100

    update

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $request->password,
             'role_id' => 1
         ]);
         event(new Registered($user));
